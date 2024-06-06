be5031c09711da5c103b9803a3371f63274ad554
Fernando Chicaiza <washopilot@yahoo.com>
2024-06-06 08:48:37 -0500

    add nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from '@/app/provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -15,8 +16,10 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang='en'>
-            <body className={inter.className}>{children}</body>
+        <html lang='en' className='light'>
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
         </html>
     )
 }
