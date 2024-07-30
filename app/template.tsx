5ffc5f2af2b745e2cc079275fea857ccb675f31e
Fernando Chicaiza <washopilot@yahoo.com>
2024-07-29 23:40:59 -0500

    add profile picture

--- app/template.tsx
+++ app/template.tsx
@@ -13,7 +13,7 @@ const Template = ({ children }: { children: React.ReactNode }) => {
     const pathname = usePathname()
 
     return (
-        <AnimatePresence mode='wait'>
+        <AnimatePresence mode='wait' initial={true}>
             <motion.section
                 key={pathname}
                 initial='hidden'
