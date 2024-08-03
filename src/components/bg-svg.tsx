f697116cc4c94cc81b7dc385e9e9553278ba9928
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-02 23:33:25 -0500

    better positions bg svgs

--- src/components/bg-svg.tsx
+++ src/components/bg-svg.tsx
@@ -1,16 +1,11 @@
-'use client'
-
 import BgLeftSVG from '@/components/bg-left'
 import { SmallLogo } from '@/components/home/logo'
-import { useTheme } from 'next-themes'
 
 const BackgroundSVG = () => {
-    const { theme, setTheme } = useTheme()
-
     return (
         <>
-            <BgLeftSVG className='fixed z-[-1] w-96 h-auto bottom-0 left-0 scale-[4] blur-3xl' />
-            <SmallLogo className='fixed z-[-10] w-[40vw] h-auto -top-[10vh] -right-[10vw] text-[#020d24] opacity-50' />
+            <BgLeftSVG className='fixed z-[-1] w-1/2 h-auto bottom-0 -left-16 scale-[2] blur-2xl' />
+            <SmallLogo className='fixed z-[-1] w-auto h-1/2 -top-16 -right-16 text-secondary-100 opacity-50 blur-sm' />
         </>
     )
 }
