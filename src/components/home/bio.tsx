daa6fa12641927002cbbf90bd4c1801af6a755cd
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-03 12:50:20 -0500

    better bio 2

--- src/components/home/bio.tsx
+++ src/components/home/bio.tsx
@@ -2,13 +2,13 @@ const Bio = () => {
     return (
         <article className='flex w-full pt-16 items-center flex-wrap gap-2 sm:gap-6 md:gap-x-10 text-justify hyphens-auto leading-normal'>
             <h3 className='text-2xl font-bold underline underline-offset-8 decoration-danger-300 decoration-4'>
-                Acerca de
+                Sobre mí
             </h3>
             <p className='text-normal font-light'>
                 Soy un desarrollador full-stack freelance radicado en Loja, Ecuador. Apasionado por la tecnología y la
-                creación de soluciones digitales innovadoras, me especializo en el desarrollo integral de servicios
-                digitales, desde la conceptualización y el diseño hasta la implementación y optimización, enfocándome en
-                resolver problemas reales a través de mi experiencia en la programación.
+                creación de soluciones digitales, me especializo en el desarrollo integral de productos tecnológicos
+                desde su conceptualización y diseño hasta la implementación y optimización, enfocándome en resolver
+                problemas reales a través de la programación.
             </p>
         </article>
     )
