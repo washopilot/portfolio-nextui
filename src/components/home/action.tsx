c46728d57aa40557d21630bc81cb2ec1208976b4
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-03 15:58:59 -0500

    apply theming button CTA

--- src/components/home/action.tsx
+++ src/components/home/action.tsx
@@ -8,11 +8,12 @@ const Action = () => {
     const router = useRouter()
     return (
         <Button
-            className='w-1/2 m-auto'
+            className='w-1/2 m-auto bg-secondary-50 text-secondary-700'
             color='secondary'
             size='lg'
             endContent={<BxRightArrowCircleIcon size={48} />}
-            onClick={() => router.push('/projects')}>
+            onClick={() => router.push('/projects')}
+            variant='faded'>
             Mi Portafolio
         </Button>
     )
