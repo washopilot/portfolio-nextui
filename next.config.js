25da7f2da77972c00c6001561a8c39f2b5d3e00a
Fernando Chicaiza <washopilot@yahoo.com>
2024-09-11 23:42:13 -0500

    fix dependencies and errors contentlayer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from 'next-contentlayer2'
+const { withContentlayer } = require('next-contentlayer2')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,4 +7,4 @@ const nextConfig = {
     reactStrictMode: true
 }
 
-export default withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig)
