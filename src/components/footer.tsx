58babdc37bd850070d606889f1ccc01d0db18343
Fernando Chicaiza <washopilot@yahoo.com>
2024-10-01 15:56:17 -0500

    upgrade dependencies

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
     return (
         <footer className='container mx-auto max-w-7xl pt-16 pb-4 px-12'>
             <div className='flex flex-col justify-center items-center gap-0'>
-                <p className='text-sm text-default-400'>© {getCurrentYear()} Fernando Chicaiza. Developer.</p>
+                <p className='text-sm text-default-400'>© {getCurrentYear()} Fernando Chicaiza. Web Developer.</p>
                 <p className='text-sm text-default-400'>
                     Con el soporte de
                     <Link isExternal isBlock href='https://nextui.org' className='py-0'>
