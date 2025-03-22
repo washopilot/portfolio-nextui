bf1bd50bbf5a96b82308587228be3a4097032f21
Fernando Chicaiza <washopilot@yahoo.com>
2025-03-21 21:58:48 -0500

    more blur in background svg

--- src/components/bg-svg.tsx
+++ src/components/bg-svg.tsx
@@ -4,8 +4,8 @@ import { SmallLogo } from '@/components/home/logo'
 const BackgroundSVG = () => {
     return (
         <>
-            <BgLeftSVG className='fixed z-[-1] w-1/2 h-auto bottom-0 -left-16 scale-[2] blur-2xl' />
-            <SmallLogo className='fixed z-[-1] w-auto h-1/2 -top-16 -right-16 text-secondary-100 opacity-50 blur-sm' />
+            <BgLeftSVG className='fixed z-[-1] w-1/2 h-auto bottom-0 -left-16 scale-[2] blur-3xl' />
+            <SmallLogo className='fixed z-[-1] w-auto h-1/2 -top-16 -right-16 text-secondary-100 opacity-50 blur-lg' />
         </>
     )
 }
