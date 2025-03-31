8836e8597780767ec02192948ce807f302f4be2b
Fernando Chicaiza <washopilot@yahoo.com>
2025-03-30 20:04:46 -0500

    change color cta-button

--- src/components/home/cta-button.tsx
+++ src/components/home/cta-button.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { BxRightArrowIcon } from '@/components/icons/arrow-right'
-import { Button } from "@heroui/react"
+import { Button } from '@heroui/react'
 import NextLink from 'next/link'
 
 const CTAButton = () => {
     return (
         <Button
             as={NextLink}
-            className='m-auto bg-secondary-800 text-content1 font-bold px-16 border border-foreground-500 rounded-medium py-6'
+            className='m-auto bg-secondary-400 text-content1-foreground font-bold px-16 border border-foreground-500 rounded-medium py-6'
             endContent={<BxRightArrowIcon className='scale-[4]' />}
             href='/projects'>
             MIS PROYECTOS&nbsp;
