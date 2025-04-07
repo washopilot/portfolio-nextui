c0bcbbd1420cffb8eeff610a1304aa9923ba3986
Fernando Chicaiza <washopilot@yahoo.com>
2025-04-07 12:21:58 -0500

    add about paragraph translate

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,40 +1,41 @@
+import profilePic from '@/../public/profile.jpg'
 import Paragraph from '@/components/home/paragraph'
 import Profile from '@/components/home/profile'
+import { getDictionary } from '@/get-dictionaries'
+import { Locale } from '@/i18n-config'
 import { Link } from '@heroui/react'
-import profilePic from '@/../public/profile.jpg'
+import ReactMarkdown from 'react-markdown'
 
-export default function AboutPage() {
+const AboutPage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
+    const { about } = await getDictionary(lang) // en
+
     return (
         <article className='flex w-full pt-16 flex-col gap-8 sm:gap-10 md:gap-12 text-justify hyphens-auto leading-normal'>
-            <Profile profilePic={profilePic} title='Fernando Chicaiza' subtitle='Desarrollador / Artista / Diseñador' />
-            <Paragraph title='Sobre mí'>
-                Soy un desarrollador freelance apasionado, con más de cuatro años de experiencia en el desarrollo web y
-                móvil. A lo largo de mi carrera, he entregado proyectos de alta calidad para diversos clientes,
-                utilizando un stack tecnológico versátil tanto en frontend como en backend. Mi experiencia se centra
-                en&nbsp;<strong>Laravel y React</strong>, pero siempre estoy abierto a aprender y adoptar nuevas
-                tecnologías. He trabajado con múltiples clientes en mi país de origen, Ecuador, lo que me ha permitido
-                adquirir una valiosa experiencia y un profundo conocimiento de las mejores prácticas en desarrollo web.
-                <br /> <br /> Mi compromiso con la excelencia y la mejora continua me impulsa a ofrecer soluciones
-                innovadoras y eficientes en cada proyecto. Me apasiona aprender nuevas herramientas y tecnologías,
-                trabajar en equipo y resolver desafíos complejos. La comunicación efectiva, la colaboración y el
-                pensamiento analítico son clave en mi enfoque de trabajo. ¡Construyamos algo increíble juntos!
-                <br /> <br /> Amo lo que hago y siempre busco nuevas formas de crecer y mejorar. La satisfacción del
-                cliente es mi máxima prioridad, y me enorgullece crear soluciones eficientes, escalables y visualmente
-                impactantes. Si buscas un profesional comprometido y con gran potencial, soy la persona indicada
-                <Link
-                    isBlock
-                    showAnchorIcon
-                    isExternal
-                    href='/resume.pdf'
-                    className='font-bold italic text-secondary-500'>
-                    <span>&nbsp;... mi CV</span>
-                </Link>
+            <Profile profilePic={profilePic} title='Fernando Chicaiza' subtitle={about.profile_subtitle} />
+            <Paragraph title={about.paragraph_title}>
+                <ReactMarkdown
+                    components={{
+                        a: ({ node, ...props }) => (
+                            <Link
+                                isBlock
+                                showAnchorIcon
+                                isExternal
+                                href={props.href}
+                                className='font-bold italic text-secondary-500'>
+                                {props.children}
+                            </Link>
+                        )
+                    }}>
+                    {about.paragraph_content}
+                </ReactMarkdown>
             </Paragraph>
             <Paragraph title='Destrezas y tecnologías'>
-                JavaScript / TypeScript / React / React Native / Next.js / Vue.js / Nuxt / Laravel / PHP / MySQL / HTML5
-                / CSS / Tailwind / Diseño Responsive & Mobile-First
+                JavaScript / TypeScript / React / React Native / Next.js / Laravel / PHP / MySQL / HTML5 / CSS /
+                Tailwind / Diseño Responsive & Mobile-First
             </Paragraph>
             <Paragraph title='Lenguajes'>Javascript/Typescript, PHP &amp; Python</Paragraph>
         </article>
     )
 }
+
+export default AboutPage
